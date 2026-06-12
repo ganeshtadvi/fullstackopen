4c9ad5cc31967ac6aed411b1f9a4368c3fd61247
The Coder Ganesh <101275670+ganeshtadvi@users.noreply.github.com>
2026-06-12 18:29:57 +0530

    Change <li> to <p> in Number component
    Refactor Number component to use <p> for contact details.

--- part_2/Phonebook/src/components/Number.jsx
+++ part_2/Phonebook/src/components/Number.jsx
@@ -1,8 +1,7 @@
 const Number = ({ id,name,number,deleteContact }) => {
   return (
   <div>
-  <li key={id}>{name} {number}</li>
-  <button onClick={deleteContact}>delete</button>
+  <p key={id}>{name} {number} <button onClick={deleteContact}>delete</button></p>
   </div>)
 };
 
